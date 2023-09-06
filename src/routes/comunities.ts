323b0de80bf7931f4884da011ddc03e587de733d
Santiago Arrascaeta <santiago7601a@gmail.com>
2023-09-06 18:51:05 -0300

    no corre la ruta

--- src/routes/comunities.ts
+++ src/routes/comunities.ts
@@ -2,14 +2,12 @@ import express from "express"
 
 const router = express.Router()
 
-router.get('/', (_req,res) => {
+router.get('/', (_req, res) => {
     res.send('Comunidades')
-
 })
 
-router.post('/', (_req,res) => {
-    res.send('guardando comunidades')
-    
+router.post('/', (_req, res) => {
+    res.send('guardando comunidades')    
 })
 
 export default router
---
+++ src/types.d.ts
@@ -0,0 +1,2 @@
+export type Weather = string
+export type Cosas = 'poner aca lo que quiera de variables'
